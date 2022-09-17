699a1187738dfb8b9121e81aeaeb55bfb0b3791f
Seth Randall <sethjrandall@gmail.com>
2022-09-17 10:28:15 -0400

    File io example

--- phone_book.c
+++ phone_book.c
@@ -1,4 +1,13 @@
-int main()
+#include <stdio.h>
+
+main()
 {
+  FILE *fp;
+
+  fp = fopen("/tmp/test.txt", "w+");
+  fprintf(fp, "This is testing for fprintf...\n");
+  fputs("This is testing for fputs...\n", fp);
+  fclose(fp);
+
   return 0;
 }
